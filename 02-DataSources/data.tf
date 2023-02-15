e84f36878cba244afd047b7906ba54a38910ead8
ADMIN <sreeinbox@gmail.com>
2023-02-15 12:15:32 +0530

    terraform datasources demo particular attribute

--- 02-DataSources/data.tf
+++ 02-DataSources/data.tf
@@ -1,11 +1,11 @@
-data "aws_ami" "example" {
+data "aws_ami" "centos8" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value=data.aws_ami.example
+  value=data.aws_ami.centos8
 }
 
 data "aws_instance" "foo" {
